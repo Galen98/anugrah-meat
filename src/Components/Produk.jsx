8b97794821e831a0d5a3ad6d809e999076202326
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-01-16 01:51:27 +0700

    first commit

--- src/Components/Produk.jsx
+++ src/Components/Produk.jsx
@@ -49,7 +49,9 @@ export default function Produk(){
     </center>
     ))}
     </div>
-    <a href="" className="justify-center mt-10 text-gray-900 bg-white border border-gray-300 focus:outline-none 
+    <br/>
+    <br/>
+    <a href="" className="justify-center text-gray-900 bg-white border border-gray-300 focus:outline-none 
     hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 
     dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 rounded-xl
     dark:focus:ring-gray-700">Lihat produk selengkapnya</a>
